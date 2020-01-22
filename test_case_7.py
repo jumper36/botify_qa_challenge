b895e8b015ca5c762159aa1d96de171daa642e4a
kmezui <kmezui@ingenico.com>
2020-01-22 23:02:59 +0100

    adding the implementation for test case 7

---
+++ test_case_7.py
@@ -0,0 +1,14 @@
+import unittest
+from random import randint
+from calculusmodule import *
+
+
+class TestCase7(unittest.TestCase):
+
+    def setUp(self):
+        self.a = randint(0, 100)
+        self.b = randint(0, 100)
+
+    def test_Multiplication(self):
+        self.assertEqual(multiplication(self.a, self.b), self.a * self.b)
+
