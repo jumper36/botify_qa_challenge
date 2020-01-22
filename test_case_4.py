31f341788f0606548fa37d86e938ff28cd31d06d
kmezui <kmezui@ingenico.com>
2020-01-22 22:28:15 +0100

    adding implementation for test case 4

---
+++ test_case_4.py
@@ -0,0 +1,16 @@
+import unittest
+from random import randint
+from calculusmodule import *
+
+
+class TestCase4(unittest.TestCase):
+
+    def setUp(self):
+        self.a = randint(0, 100)
+        self.b = randint(0, 100)
+
+    def test_Addition(self):
+        self.assertEqual(addition(self.a, self.b), self.a + self.b)
+
+
+
