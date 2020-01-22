478979e26f10ef16f77c38e77fa96c5a16e8b4e2
kmezui <kmezui@ingenico.com>
2020-01-22 23:03:45 +0100

    adding the implementation for test case 8

---
+++ test_case_8.py
@@ -0,0 +1,14 @@
+import unittest
+from random import randint
+from calculusmodule import *
+
+
+class TestCase8(unittest.TestCase):
+
+    def setUp(self):
+        self.a = randint(0, 100)
+        self.b = randint(0, 100)
+
+    def test_Percentage(self):
+        self.assertEqual(percentage(self.a, self.b), (self.a / self.b) * 100)
+
